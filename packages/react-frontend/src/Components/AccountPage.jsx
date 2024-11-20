1d62cb20a30bcb8adda14e6180b56ee9aab53c2e
kennethwu42 <94160443+kennethwu42@users.noreply.github.com>
2024-11-20 12:27:55 -0800

    Update AccountPage.jsx

--- packages/react-frontend/src/Components/AccountPage.jsx
+++ packages/react-frontend/src/Components/AccountPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../Styles/AccountPage.scss";
 
 const AccountPage = ({ API_PREFIX, addAuthHeader, logoutUser }) => {
