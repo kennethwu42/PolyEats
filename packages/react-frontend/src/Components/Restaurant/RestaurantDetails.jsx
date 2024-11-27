e73b2805f3524b7c4b6260e425026a3094e30353
njahadi <njahadi@calpoly.edu>
2024-11-27 13:17:54 -0800

    remove React

--- packages/react-frontend/src/Components/Restaurant/RestaurantDetails.jsx
+++ packages/react-frontend/src/Components/Restaurant/RestaurantDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
