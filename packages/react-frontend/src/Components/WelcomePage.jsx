1346ec2efcbd40872666aa21cf5395eb995f039f
sofiamorris <sofiamorris213@gmail.com>
2024-12-02 13:02:38 -0800

    cleaning welcome/login

---
+++ packages/react-frontend/src/Components/WelcomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../Styles/WelcomePage.scss";
+
+const WelcomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="welcome-page">
+      <div className="welcome-buttons">
+        <button className="welcome-btn" onClick={() => navigate("/login")}>
+          Login
+        </button>
+        <button className="welcome-btn" onClick={() => navigate("/signup")}>
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
